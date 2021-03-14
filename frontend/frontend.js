c020c907971279e090b44c8679ff0268c9e782ea
Aikanysh Kerimkulova <aikanysh.kerimkulova@gmail.com>
2021-03-14 21:32:34 +0600

    add button addToCartBtn

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,6 +1,27 @@
 import { getSweets } from "./helper.js";
 
+let cart = [];
 let $sweets = document.querySelector("#sweets");
+let $cart = document.querySelector("#cart");
+
+const renderCart = () => {
+  $cart.innerHTML = "";
+  cart.forEach(({ id, name: sweetName, cost }) => {
+    $cart.insertAdjacentHTML(
+      "beforeend",
+      `<div class="cart__item row">
+            <div class="col-8">
+            ${sweetName}</div>
+            <div class="col-4">
+            ${cost}</div>
+        </div>`
+    );
+  });
+};
+const addToCart = (sweet) => {
+  cart.push(sweet);
+  renderCart();
+};
 
 const renderSweetElement = (sweet) => {
   let $html = document.createElement("div");
@@ -12,6 +33,18 @@ const renderSweetElement = (sweet) => {
     <p class = 'sweet__ingredients'>${sweet.ingredients.join(", ")}</p>
     <p class = 'sweet__price'>$${sweet.cost}</p>
     `;
+
+  let $addToCartBtn = document.createElement("button");
+  if (sweet.inStock === 0) {
+    $addToCartBtn.setAttribute("disabled", "disabled");
+  }
+  $addToCartBtn.textContent =
+    sweet.inStock == 0 ? "Not Available" : "Add to cart";
+  $addToCartBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    addToCart(sweet);
+  });
+  $html.append($addToCartBtn);
   return $html;
 };
 const renderSweets = async () => {
